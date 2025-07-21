2312bd9c068c2a5384443a56165276e95eb7fd6a
Bhavadeesh18 <bhavadeeshreddynandyala2003@gmail.com>
2025-07-21 16:07:20 +0530

    First Commit

---
+++ HelloWorld/HelloWorld/Custominput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Custominput
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: "); // Prompt user for input
+            string name = Console.ReadLine();   // Read input from console
+
+            Console.WriteLine($"Hello, {name}! Welcome to .NET."); // Print greeting
+        }
+    }
+}
