40edbccf1f4d1f4e7518f334411407ed1288872b
Bhavadeesh18 <bhavadeeshreddynandyala2003@gmail.com>
2025-07-22 17:02:58 +0530

    Part2 Commit Jul22

---
+++ Day2Practice/Day2Practice/Arrayrevalt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "welcome to dotnet programming";
+        string[] words = input.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (i % 2 != 0) // Reverse alternate words (odd indices)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+        }
+
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
+    }
+}
---
+++ Day2Practice/Day2Practice/Milestone2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Program to separate string after space and add to array
+/// </summary>
+namespace DemoApplication
+{
+    internal class MileStoneEx2
+    {
+        static void Main()
+        {
+            string str = "Welcome to dotnet programming trainer prasanna";
+            string[] names = str.Split(' ');
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+
